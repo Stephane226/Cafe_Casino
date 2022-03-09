8b59e53b8850dca4d465dd965934241238c50e5f
Masaaki Stephane <bstephane97@yandex.com>
2022-03-09 23:17:45 +0300

    Home  page content building(section 1)

---
+++ body_content.php
@@ -0,0 +1,210 @@
+<section class="home-content">
+ 
+ <div class="container home-block">
+  <div class="row">
+    <div class="col-lg-8">
+        <div class="inner-block">
+      <h1>Overview & Details</h1>
+       <div class="home-overview">
+
+        <div class="overview-case">
+               <h6>License</h6>
+              <span> Vuracao</span>
+        </div>
+
+        <div class="overview-case">
+               <h6>Withdraw</h6>
+              <span> Up to 7 days</span>
+        </div>
+
+
+        <div class="overview-case">
+               <h6>Languages</h6>
+              <span> French, English, Germain, Danish, Polish</span>
+        </div>
+
+        <div class="overview-case">
+               <h6>Casino Features</h6>
+              <span> Blackjack, Casicshot,Jackpot Games,Live Casino, Pay'n Play, Roullette, Table Game,Video Pocker
+                  Video Slots, VIP Programs
+              </span>
+        </div>
+
+        <div class="overview-case">
+               <h6>Estabilished</h6>
+              <span> 2021</span>
+        </div>
+
+
+        <div class="overview-case">
+               <h6>Withdrawal Limits</h6>
+              <span> €10000 per month</span>
+        </div>
+
+        <div class="overview-case">
+               <h6>Bonus</h6>
+              <span> 100% up to €300 + 100 free spins</span>
+        </div>
+
+
+        </div>
+
+
+
+
+
+
+
+
+        <div class="inner-block">
+      <h1>Bonus Information</h1>
+       <div class="home-overview">
+
+        <div class="overview-case">
+               <h6>First Deposit Bonus</h6>
+              <span>> 100% to €300</span>
+        </div>
+
+        <div class="overview-case">
+               <h6>Free Sping Wagerin</h6>
+              <span> x20</span>
+        </div>
+
+
+        <div class="overview-case">
+               <h6>Bonus Wagerin</h6>
+              <span> 40x</span>
+        </div>
+
+        <div class="overview-case">
+               <h6>First Deposit Free Spins</h6>
+              <span> 300
+              </span>
+        </div>
+
+        <div class="overview-case">
+               <h6>Max Bonus Bet</h6>
+              <span> €3</span>
+        </div>
+
+
+        <div class="overview-case">
+               <h6>Bonus Code</h6>
+              <span> N/A</span>
+        </div>
+
+        <div class="overview-case">
+               <h6>Bonus</h6>
+              <span> 100% up to €300 + 100 free spins</span>
+        </div>
+
+
+        </div>
+      </div>
+      
+
+
+
+      <div class="inner-block">
+      <h1>Payement Mehtods</h1>
+       <div class="home-overview">
+
+       
+
+        <div class="overview-case">
+               <h6>Depositing Method</h6>
+              <span> French, English, Germain, Danish, Polish</span>
+        </div>
+
+        <div class="overview-case">
+               <h6>Withdrawal Method</h6>
+              <span> Blackjack, Casicshot,Jackpot Games,Live Casino, Pay'n Play, Roullette, Table Game
+              </span>
+        </div>
+
+        </div>
+        </div>
+
+
+
+
+        <div class="inner-block">
+      <h1>Normal Context Text</h1>
+       <div class="">
+
+       
+
+        <div class="overview-p">
+              <p>
+              Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie
+              mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie
+              </p>
+        </div>
+
+        <div class="overview-p">
+              <p>
+              Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie
+              mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie
+              </p>
+        </div>
+
+        <div class="overview-p">
+              <p>
+              Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie
+              mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie
+              </p>
+        </div>
+
+       
+
+
+        <div class="inner-block">
+    
+       <div class="home-overview-btn">
+
+       <h4>Players Also Like</h4>
+
+        <div class="overview-case">
+               <h6>Depositing Method</h6>
+              <span> French, English, Germain, Danish, Polish</span>
+        </div>
+
+        <div class="overview-case">
+               <h6>Withdrawal Method</h6>
+              <span> Blackjack, Casicshot,Jackpot Games,Live Casino, Pay'n Play, Roullette, Table Game
+              </span>
+        </div>
+
+        </div>
+        </div>
+
+
+
+
+
+
+
+
+      </div> 
+    </div>
+
+    </div>
+   </div>
+
+
+
+
+
+
+
+
+
+    <div class="col-lg-4">
+      <h4> Table Of Content</h4>
+    
+    </div>
+  </div>
+  </div>
+
+
+</section>
